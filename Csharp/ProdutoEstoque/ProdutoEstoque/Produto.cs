75a301179041a4546fe62cc1e65639feb1f59633
Andre Alencar <aaugusto310@gmail.com>
2021-02-19 07:16:02 -0300

    Criação e comclusão da classe Produto

---
+++ Csharp/ProdutoEstoque/ProdutoEstoque/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProdutoEstoque
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public double ValorEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+            Console.WriteLine($"Dados atualizados: {Nome}, {Preco.ToString("C")}, {Quantidade} unidades, Total: {ValorEmEstoque().ToString("C")} ");
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+            Console.WriteLine($"Dados atualizados: {Nome}, {Preco.ToString("C")}, {Quantidade} unidades, Total: {ValorEmEstoque().ToString("C")} ");
+        }
+
+        public string InfoProduto()
+        {
+            return $"Dados do produto: {Nome}, {Preco.ToString("C")}, {Quantidade} unidades, Total: {ValorEmEstoque().ToString("C")}";
+        }
+    }
+}
