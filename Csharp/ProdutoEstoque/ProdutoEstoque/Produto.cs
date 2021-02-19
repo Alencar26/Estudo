a4fc493e69996d24ebd352055ff1a5acf23779b9
Andre Alencar <aaugusto310@gmail.com>
2021-02-19 07:27:43 -0300

    adicionado método ToString()

--- Csharp/ProdutoEstoque/ProdutoEstoque/Produto.cs
+++ Csharp/ProdutoEstoque/ProdutoEstoque/Produto.cs
@@ -16,18 +16,23 @@ namespace ProdutoEstoque
         public void AdicionarProdutos(int quantidade)
         {
             Quantidade += quantidade;
-            Console.WriteLine($"Dados atualizados: {Nome}, {Preco.ToString("C")}, {Quantidade} unidades, Total: {ValorEmEstoque().ToString("C")} ");
+            Console.WriteLine($"Dados atualizados: {ToString()}");
         }
 
         public void RemoverProdutos(int quantidade)
         {
             Quantidade -= quantidade;
-            Console.WriteLine($"Dados atualizados: {Nome}, {Preco.ToString("C")}, {Quantidade} unidades, Total: {ValorEmEstoque().ToString("C")} ");
+            Console.WriteLine($"Dados atualizados: {ToString()}");
         }
 
         public string InfoProduto()
         {
-            return $"Dados do produto: {Nome}, {Preco.ToString("C")}, {Quantidade} unidades, Total: {ValorEmEstoque().ToString("C")}";
+            return $"Dados do produto: {ToString()}";
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Preco.ToString("C")}, {Quantidade} unidades, Total: {ValorEmEstoque().ToString("C")}";
         }
     }
 }
