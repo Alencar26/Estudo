33f7a41ad82d27580c8f538662bc67e8c56d9047
Andre Alencar <aaugusto310@gmail.com>
2021-02-24 17:41:44 -0300

    projeto de estudo de List, implementação de alguns exercícios para fixação do conteúdo.

---
+++ Csharp/Listas/Listas/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+
+            lista.Add("Hello World");
+            lista.Add("Hello World");
+            lista.Add("Hello World");
+            lista.Add("Hello World");
+            lista.Add("Hello World");
+
+            // diferença do insert é que ele aceita informar qual será a posição
+            // que será inserido os dados.
+            lista.Insert(3, "Olá Mundo");
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Quantidade de itens na lista: {lista.Count}");
+
+            // ==================== CRIANDO LISTA COM ELEMENTOS =======================
+
+            List<string> lista2 = new List<string>() { "String1", "String2", "String3" };
+
+            // ============================== FIND ==========================================
+
+            string br = lista.Find(x => x[0] == 'O');
+            Console.WriteLine($"String em pt-Br: {br}");
+
+            // ============================= REMOVE =======================================
+
+            // removendo pelo nome do elemento!
+            lista.Remove("Olá Mundo");
+
+            //removendo pela posição
+            lista.RemoveAt(3);
+
+            // remover elementos em uma faixa determinada
+            lista.RemoveRange(2, 3); // << a partir da posição 2, quero remover 3 elementos
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+        } 
+    }
+}
