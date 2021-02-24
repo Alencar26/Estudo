054918e130a44cfa0ae875b90e48103096c3587b
Andre Alencar <aaugusto310@gmail.com>
2021-02-24 16:47:48 -0300

    criação de projeto e implementação didático dos modificadores ref e out

---
+++ Csharp/Modificadores_parametro_ref_e_out/Modificadores_parametro_ref_e_out/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modificadores_parametro_ref_e_out
+{
+    public class Calculator
+    {
+        public static void Multiplica(ref int x)
+        {
+            x = x * 2;
+        }
+
+        public static void Triplica(int entrada, out int result)
+        {
+            result = entrada * 3;
+        }
+    }
+}
---
+++ Csharp/Modificadores_parametro_ref_e_out/Modificadores_parametro_ref_e_out/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Modificadores_parametro_ref_e_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Calculator.Multiplica(ref num);
+            Console.WriteLine(num);
+
+            //=================================
+
+            // modificador out, não exige que a variável seja iniciada.
+
+            int entrada = 10;
+            int resultado;
+
+            // o método abaixo vai retornar o resultado por meio de parâmetro para a variável "resultado"
+            Calculator.Triplica(entrada, out resultado);
+            Console.WriteLine(resultado);
+        }
+    }
+}
