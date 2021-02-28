10c6ee4eaeee8ebf74226694024442891c6e710c
Andre Alencar <aaugusto310@gmail.com>
2021-02-28 15:53:24 -0300

    criação do projeto EnumWorker, imlpementação da classe Worker e implementação da classe Enum WorkerLevel.

---
+++ Csharp/EnumWorker/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,10 @@
+
+namespace EnumWorker.Entities.Enums
+{
+    public enum MyEnumType : int
+    {
+        JUNIOR,
+        MID_LEVEL,
+        SENIOR
+    }
+}
---
+++ Csharp/EnumWorker/Entities/Worker.cs
@@ -0,0 +1,16 @@
+using System;
+using EnumWorker.Entities.Enums;
+
+namespace EnumWorker.Entities
+{
+    public class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        
+        
+        
+        
+    }
+}
