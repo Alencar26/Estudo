58bc3a7ee373582eec9a38639b783f78302a163a
Andre Alencar <aaugusto310@gmail.com>
2021-03-02 17:39:24 -0300

    implementando a palavra reservada sealed que informa que uma classe ou método sobrepostos não possam ser herdadas.

--- Csharp/Heranca/Heranca/Entities/SavingsAccount.cs
+++ Csharp/Heranca/Heranca/Entities/SavingsAccount.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Heranca.Entities
 {
-    public class SavingsAccount : Account
+    public sealed class SavingsAccount : Account
     {
         public double InterestRate { get; set; }
 
@@ -20,7 +20,7 @@ namespace Heranca.Entities
             Balance += Balance * InterestRate;
         }
 
-        public override void Withdraw(double amount)
+        public sealed override void Withdraw(double amount)
         {
             Balance -= amount; 
         }
