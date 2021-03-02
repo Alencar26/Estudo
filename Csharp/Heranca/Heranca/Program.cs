d2bffcfda0b0f3ac6ff93ce8b7047f87e872fc36
Andre Alencar <aaugusto310@gmail.com>
2021-03-02 14:44:59 -0300

    Upcasting e Downcasting, conversão dos tipos com (casting) e palavrinha as.

--- Csharp/Heranca/Heranca/Program.cs
+++ Csharp/Heranca/Heranca/Program.cs
@@ -8,9 +8,36 @@ namespace Heranca
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Alencar", 100.0, 500.0);
+            Account acc = new Account(2001, "Ana", 10.0);
+            BusinessAccount bacc = new BusinessAccount(2002, "Alencar", 100.0, 500.0);
 
-            Console.WriteLine(account.Balance);
+            //CONVERSÕES
+            // (conversão da subclasse para a super classe)
+            // UPCASTING
+            Account acc1 = bacc; // isso é válido! já que o  business Account é um Account
+            Account acc2 = new BusinessAccount(2002, "Alencar", 100.0, 500.0);
+            Account acc3 = new SavingsAccount(2003, "Helô", 10000, 0.05);
+
+
+            //DOWNCASTING
+
+            // isso não funciona
+            //BusinessAccount acc4 = acc2;
+
+            BusinessAccount acc4 = (BusinessAccount) acc2; // tem que fazer um casting (conversão)
+
+            //NÃO PODE FAZER ISSO!! nÃO SÃO COMPATÍVEIS
+            BusinessAccount acc5 = (BusinessAccount)acc3; // <<< VAI OCORRER UM ERRO NA HORA DE EXECUTAR O PROGRAMA.
+
+            //verificando se a instância  da variável é do tipo BusinessAccount 
+            if (acc3 is BusinessAccount)
+            {
+                BusinessAccount acc6 = (BusinessAccount)acc3;
+            }
+
+            // sintaxe alternativa para converter (casting) o tipo da variável.
+            SavingsAccount acc7 = acc3 as SavingsAccount;
+
         }
     }
 }
