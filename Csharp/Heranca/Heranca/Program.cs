b4366e5d4b22676dca95f8c8860d1e0d7d49367a
Andre Alencar <aaugusto310@gmail.com>
2021-03-02 11:18:59 -0300

    implementação de herança com reaproveitamento do contrutor da classe super e modificador de acesso protected.

---
+++ Csharp/Heranca/Heranca/Program.cs
@@ -0,0 +1,17 @@
+﻿using Heranca.Entities;
+using System;
+
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Alencar", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
+  
