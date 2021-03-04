395b65433c161403c9bb2963fad01caf74a01339
Andre Alencar <aaugusto310@gmail.com>
2021-03-04 10:06:23 -0300

    Relembrando tratamento de exceções com try, catch e throw... Criando exceções personalilzadas com (ApplicationException).

---
+++ Csharp/Excecoes/Excecoes/Entities/Reservation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Excecoes.Entities.Exceptions;
+
+namespace Excecoes.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            if (checkout <= checkin)
+            {
+                throw new DomainException("A data de checkout é anterior à data de checkin");
+            }
+
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            return (int) (Checkout.Day - Checkin.Day);
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            DateTime now = DateTime.Now;
+            if (checkin < now || checkout < now)
+            {
+                throw new DomainException("As datas de atualização não podem ser anterior à data de agora.");
+            }
+            if (checkout <= checkin)
+            {
+                throw new DomainException("A data de checkout é anterior à data de checkin");
+            }
+
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return $"ROOM {RoomNumber}, check-in: {Checkin.ToString("dd/MM/yyyy")}," +
+                   $" check-out: {Checkout.ToString("dd/MM/yyyy")}, {Duration()} nights";
+        }
+    }
+}
---
+++ Csharp/Excecoes/Excecoes/Program.cs
@@ -0,0 +1,40 @@
+﻿using Excecoes.Entities;
+using Excecoes.Entities.Exceptions;
+using System;
+
+namespace Excecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Romm Number: ");
+                int rN = int.Parse(Console.ReadLine());
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime cI = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime cO = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(rN, cI, cO);
+                Console.WriteLine(reservation);
+
+                Console.WriteLine("Entre com os dados para atualizar a reserva: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                cI = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                cO = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(cI, cO);
+                Console.WriteLine(reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Erro na reserva: {e.Message}");
+            }
+        }
+    }
+}
