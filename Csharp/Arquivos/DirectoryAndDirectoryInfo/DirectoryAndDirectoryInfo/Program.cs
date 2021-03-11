6dbe225cb82854c36133400a184b59d2bd423daa
Andre Alencar <aaugusto310@gmail.com>
2021-03-11 13:14:34 -0300

    Entendendo as classes Directory e DirectoryInfo. Ciração de pastas, e listagemde diretórios e arquivos.

---
+++ Csharp/Arquivos/DirectoryAndDirectoryInfo/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DirectoryAndDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\andre\www\ws_csarp\Exercicios_POO\Csharp\Arquivos\pastaDeTeste";
+
+            try
+            {
+                // listando o caminho de todas as pastar(inclusive aninhadas)
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string f in folders)
+                {
+                    Console.WriteLine(f);
+                }
+
+                // listando o caminho de todos os arquivos
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string f in files)
+                {
+                    Console.WriteLine(f);
+                }
+
+                //criando uma pasta
+                Directory.CreateDirectory($@"{path}\newFolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("houve um erro:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
