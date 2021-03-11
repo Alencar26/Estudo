1a8e8900d07c1e9f897764700d65109793a7ce42
Andre Alencar <aaugusto310@gmail.com>
2021-03-10 22:11:17 -0300

    usando o bloc using para que ele lide com aberturas de arquivos de forma automatica e simplificada.

--- Csharp/Arquivos/FileStreamAndFileReader/FileStreamAndFileReader/Program.cs
+++ Csharp/Arquivos/FileStreamAndFileReader/FileStreamAndFileReader/Program.cs
@@ -9,27 +9,30 @@ namespace FileStreamAndFileReader
         {
             string path = @"C:\Users\andre\www\ws_csarp\Exercicios_POO\Csharp\Arquivos\File1.txt";
 
-           // FileStream fs = null;
-           // StreamReader sr = null;
+
+            // SEM USER O BLOCO USING
+            /*
+            FileStream fs = null;
+            StreamReader sr = null;
             StreamReader srResumido = null;
 
 
             try
             {
-                // instanciando o FileStream passando por argumento o caminho e o modo como eu quero lidar com o arquivo
-               // fs = new FileStream(path, FileMode.Open);
-               // sr = new StreamReader(fs);
+                //instanciando o FileStream passando por argumento o caminho e o modo como eu quero lidar com o arquivo
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
 
-               // string line = sr.ReadLine();
-               // Console.WriteLine(line);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
 
                 // forma resumida de abrir um arquivo.
                 srResumido = File.OpenText(path);
 
                 while (!srResumido.EndOfStream)
                 {
-                    string line = srResumido.ReadLine();
-                    Console.WriteLine(line);
+                    string linha = srResumido.ReadLine();
+                    Console.WriteLine(linha);
                 }
 
             }
@@ -37,12 +40,35 @@ namespace FileStreamAndFileReader
             {
                 Console.WriteLine($"Houve um erro: ");
                 Console.WriteLine(e.Message);
-            } finally
+            }
+            finally
             {
-              //  if (sr != null) sr.Close();
-              //  if (fs != null) fs.Close();
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
                 if (srResumido != null) srResumido.Close();
             }
+            */
+
+
+            // COM O BLOCO USING
+
+            try
+            {
+                using (StreamReader fs = File.OpenText(path))
+                {
+                    while (!fs.EndOfStream)
+                    {
+                        string line = fs.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Houve um erro: ");
+                Console.WriteLine(e.Message);
+            }
+            
 
         }
     }
