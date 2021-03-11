d8ba594d4f62575bb59c5145707582c3d53f005d
Andre Alencar <aaugusto310@gmail.com>
2021-03-11 13:31:29 -0300

    Algumas função com a classe Path.

---
+++ Csharp/Arquivos/PathExample/PathExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace PathExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:caminho_do_arquivo\Arquivos\pastaDeTeste\File1.txt";
+
+            Console.WriteLine("Caracter de separação: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("caracter de separação de pastas: " + Path.PathSeparator);
+            Console.WriteLine("Nome do diretório do arquivo: " + Path.GetDirectoryName(path));
+            Console.WriteLine("Nome do arquivo: " + Path.GetFileName(path));
+            Console.WriteLine("Extenção do arquivo: " + Path.GetExtension(path));
+            Console.WriteLine("Nome do arquivo sem a extenção: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Pasta temporária do sistema: " + Path.GetTempPath());
+        }
+    }
+}
