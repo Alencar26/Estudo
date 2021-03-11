4559dec66786886c70f0fd7b211e9c5acb7c55b1
Andre Alencar <aaugusto310@gmail.com>
2021-03-11 16:01:52 -0300

    implementação de um sistema de aluguel de carro sem o uso de interface.

---
+++ Csharp/InterfaceExample/InterfaceExample/Entities/Invoice.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace InterfaceExample.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment { get { return BasicPayment + Tax; } }
+
+        public override string ToString()
+        {
+            return $"INVOICE: \n" +
+                   $"Basic payment: {BasicPayment.ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                   $"Tax: {Tax.ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                   $"Total payment {TotalPayment.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Csharp/InterfaceExample/InterfaceExample/Services/RentalService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using InterfaceExample.Entities;
+
+namespace InterfaceExample.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            double basicPayment = DurationValue(carRental.Start, carRental.Finish);
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+
+        private double DurationValue(DateTime start, DateTime finish)
+        {
+            TimeSpan duration = finish.Subtract(start);
+            if (duration.TotalHours > 12)
+                return Math.Ceiling(duration.TotalDays) * PricePerDay;
+            else
+                return Math.Ceiling(duration.TotalHours) * PricePerHour;
+        }
+
+        //Nota: Math.Ceiling() << arredonda o valor para cima.
+    }
+}
