b1eb311e400b48fefd3b5b5c1c6b7e95b0465fde
Andre Alencar <aaugusto310@gmail.com>
2021-03-12 16:36:57 -0300

    Exemplo de extension methods com c#.

---
+++ Csharp/ExtentionMethodsExample/ExtentionMethodsExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtentionMethodsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 03, 05, 15, 36, 59);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
