a03f0bb8af89f01e32a129f255b7767c156d6ca0
Andre Alencar <aaugusto310@gmail.com>
2021-03-16 15:20:23 -0300

    Action Example.

---
+++ Csharp/ActionExample/ActionExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using PredicateExample.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ActionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //list.ForEach(x => { x.Price += x.Price * 0.1; }); //<== incrementando 10% para cada roduto
+            list.ForEach(UpdatePrice); // <== Action
+
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+
+        //Action
+        public static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
