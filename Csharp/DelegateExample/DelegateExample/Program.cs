7ffedcb46ee70e66414c26105004036388120fa7
Andre Alencar <aaugusto310@gmail.com>
2021-03-16 11:29:53 -0300

    Entendendo delegates, exemplo simples.

---
+++ Csharp/DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DelegateExample.Service;
+
+namespace DelegateExample
+{
+    delegate double Operation(double x, double y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10;
+            double y = 5;
+            double result;
+
+            Operation opSum = CalculationService.Sum;
+            Operation opMax = CalculationService.Max;
+
+            result = opSum(x, y);
+
+            Console.WriteLine(result);
+        }
+    }
+}
