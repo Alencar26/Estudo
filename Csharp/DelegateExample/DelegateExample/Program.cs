376c0219d4d0fd2511c7820f9597c4656c3ea0b3
Andre Alencar <aaugusto310@gmail.com>
2021-03-16 11:39:44 -0300

    Multcast delegates. Exemplo.

--- Csharp/DelegateExample/DelegateExample/Program.cs
+++ Csharp/DelegateExample/DelegateExample/Program.cs
@@ -4,6 +4,7 @@ using DelegateExample.Service;
 namespace DelegateExample
 {
     delegate double Operation(double x, double y);
+    delegate void OperatioShow(double x, double y);
 
     class Program
     {
@@ -19,6 +20,16 @@ namespace DelegateExample
             result = opSum(x, y);
 
             Console.WriteLine(result);
+
+            //===================================================
+
+            // Multcast delegates:
+
+            OperatioShow opShow = CalculatioShowService.ShowMax;
+                         opShow += CalculatioShowService.ShowSum;
+
+            opShow(x, y);
+            //opShow.Invoke(x, y);
         }
     }
 }
---
+++ Csharp/DelegateExample/DelegateExample/Service/CalculatioShowService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateExample.Service
+{
+    class CalculatioShowService
+    {
+        public static void ShowMax(double x, double y)
+        {
+            Console.WriteLine((x > y) ? x : y);
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+}
