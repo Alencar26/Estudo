801734d67824bb481b5cde533793e9fd520d5f16
Andre Alencar <aaugusto310@gmail.com>
2021-03-16 15:34:20 -0300

    Func Example

---
+++ Csharp/FuncExample/FuncExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using PredicateExample.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ActionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(NameToUpper).ToList(); // <== Func
+
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+
+        //Func
+        static string NameToUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
