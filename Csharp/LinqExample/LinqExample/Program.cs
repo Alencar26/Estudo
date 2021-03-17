1c95877ca78cd3d3c508e487569fee3e0dcd146f
Andre Alencar <aaugusto310@gmail.com>
2021-03-17 10:57:32 -0300

    Introdução ao LINQ. (ORM)

---
+++ Csharp/LinqExample/LinqExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinqExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //my data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //my query expression
+            IEnumerable<int> result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // execute the query
+            foreach (var n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
