f8f512534d4823a8a189007bb0424a15ffc21479
Andre Alencar <aaugusto310@gmail.com>
2021-03-19 00:07:12 -0300

    Sintaxe alternativa parecida com SQL para expressões com LINQ.

--- Csharp/DemoLINQExample/DemoLINQExample/Program.cs
+++ Csharp/DemoLINQExample/DemoLINQExample/Program.cs
@@ -33,24 +33,73 @@ namespace DemoLINQExample
             products.Add(new Product(6, "Level", 70, category1));
             products.Add(new Product(7, "Saw", 80, category1));
 
+            Console.WriteLine("============================================================================");
 
             var r1 = products.Where(p => p.Price < 900 && p.Category.Tier == 1);
+
+            //sintaxe parecida com SQL
+            var r1Alternative = from p in products 
+                                where p.Category.Tier == 1 && p.Price < 900
+                                select p;
+
             Print("Tier 1 and Price < 900:", r1);
+            Print("Tier 1 and Price < 900:(ALTERNATIVE)", r1Alternative);
+
+            Console.WriteLine("============================================================================");
 
             var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+
+            var r2Alternative = from p in products
+                                where p.Category.Name == "Tools"
+                                select p.Name;
+
             Print("Names of Products from Tools", r2);
+            Print("Names of Products from Tools (ALTERNATIVE)", r2Alternative);
+
+            Console.WriteLine("============================================================================");
 
             // USANDO OBJETO ANONIMO NA EXPRESSÃO LAMBDA
             var r3 = products.Where(p => p.Name.StartsWith("C")).Select(p => new { p.Name, p.Price, AliasCategoryName = p.Category.Name });
+
+            var r3Alternative = from p in products
+                                where p.Name.StartsWith("C")
+                                select new { 
+                                    p.Name,
+                                    p.Price,
+                                    AliasCategoryName = p.Category.Name
+                                };
+
             Print("Products starts with C: ", r3);
+            Print("Products starts with C: (ALTERNATIVE)", r3Alternative);
+
+            Console.WriteLine("============================================================================");
 
             // pegando produtosque tenha o tier 1 e ordenando por valor crescente e depois por nome.
             var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+
+            var r4Alternative = from p in products
+                                where p.Category.Tier == 1
+                                orderby p.Name
+                                orderby p.Price
+                                select p;
+            
             Print("Tier 1 order by price then by name: ", r4);
+            Print("Tier 1 order by price then by name: (ALTERNATIVE)", r4Alternative);
+
+            Console.WriteLine("============================================================================");
 
             // pule 2 elementos e pegue 4 elementos
             var r5 = r4.Skip(2).Take(4);
+
+            var r5Alternative = (from p in r4Alternative
+                                select p)
+                                .Skip(2)
+                                .Take(4);
+
             Print("Skip 2 and take 4: ", r5);
+            Print("Skip 2 and take 4: (ALTERNATIVE)", r5Alternative);
+
+            Console.WriteLine("============================================================================");
 
             var r6 = products.First();
             Console.WriteLine("first: " + r6 + "\n");
@@ -66,6 +115,10 @@ namespace DemoLINQExample
             // group by
 
             var r9 = products.GroupBy(p => p.Category);
+
+            var r9Alternative = from p in products
+                                group p by p.Category;
+
             foreach (IGrouping<Category, Product> group in r9)
             {
                 Console.WriteLine("Category " + group.Key.Name + ":");
