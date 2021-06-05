ffb71d00d1695d23a000ae693c20f9087b3cdfd4
Andre Alencar <aaugusto310@gmail.com>
2021-06-05 08:57:42 -0300

    melhorando socket para não fechar o servidor quando um cliente é desconectado.

--- java/Socket/src/main/java/Server/SrvSocket.java
+++ java/Socket/src/main/java/Server/SrvSocket.java
@@ -3,6 +3,7 @@ package Server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class SrvSocket {
@@ -10,21 +11,25 @@ public class SrvSocket {
     public void execute(){
         try{
             ServerSocket server = new ServerSocket(12345);
-            System.out.println("Aguardando conexão");
-            Socket client = server.accept();
-            System.out.println("conectado!");
 
-            Scanner input = new Scanner(client.getInputStream());
-            while(input.hasNextLine()) {
-                String recebido = input.nextLine();
-                System.out.println(recebido);
+            String recebido = "";
+            while(!recebido.toUpperCase().equals("FECHAR")) {
+                System.out.println("Aguardando conexão");
+                Socket client = server.accept();
+                System.out.println("Conectou!");
+                Scanner input = new Scanner(client.getInputStream());
+                recebido = "";
+                while (!recebido.toUpperCase().equals("SAIR") && !recebido.toUpperCase().equals("FECHAR")){
+                    recebido = input.nextLine();
+                    System.out.println(recebido);
+                }
+                client.close();
             }
-
-            client.close();
             server.close();
         } catch (IOException e){
             e.printStackTrace();
         }
+        System.out.println("Server Down");
     }
 
 }
