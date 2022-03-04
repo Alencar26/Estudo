150d3256f6a14e91a9bfcd320e8c23d7c035c6d7
Andre Alencar <aaugusto310@gmail.com>
2022-03-04 19:33:59 -0300

    Ajuste classe Aluno

--- java/Streams/src/Aluno.java
+++ java/Streams/src/Aluno.java
@@ -1,9 +1,9 @@
-public class AlunoFilter {
+public class Aluno {
 
     final String nome;
     final double nota;
 
-    public AlunoFilter(String nome, double nota) {
+    public Aluno(String nome, double nota) {
         this.nome = nome;
         this.nota = nota;
     }
