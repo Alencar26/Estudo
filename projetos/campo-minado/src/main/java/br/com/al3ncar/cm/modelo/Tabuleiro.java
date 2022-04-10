e15679cfe8e8eea1d539249b43d1b2d2d32ff9dc
Andre Alencar <aaugusto310@gmail.com>
2022-04-10 14:28:47 -0300

    campo minado | Classe: Tabuleiro | implementação dos atributos, construtor e métodos para gerar cada campo do game e metodo de associação de vizinhos para cada campo.

--- projetos/campo-minado/src/main/java/br/com/al3ncar/cm/modelo/Tabuleiro.java
+++ projetos/campo-minado/src/main/java/br/com/al3ncar/cm/modelo/Tabuleiro.java
@@ -1,4 +1,43 @@
 package br.com.al3ncar.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
+    }
+
+    private void gerarCampos() {
+        for(int linha = 1; linha <= linhas; linha++) {
+            for (int coluna = 1; coluna <= colunas; coluna++) {
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarVizinhos() {
+        for(Campo campo : campos) {
+            for(Campo candidatoAVizinho : campos) {
+                campo.adicionarVizinho(candidatoAVizinho);
+            }
+        }
+    }
+
+    private void sortearMinas() {
+
+    }
 }
