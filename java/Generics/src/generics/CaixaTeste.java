3482298d0f9f2a1b26d70a1c30eb59ffc2f5ffd0
Andre Alencar <aaugusto310@gmail.com>
2022-04-20 12:20:18 -0300

    Generics | java - exemplos | aula 2 | Implementando generics 'amarrando' o tipo na hora de instanciar a classe. (Melhor que a forma anterior que necessita de Casting para converter o tipo na hora de atribuir o valor à uma variável ou algo do tipo.)

---
+++ java/Generics/src/generics/CaixaTeste.java
@@ -0,0 +1,13 @@
+package generics;
+
+public class CaixaTeste {
+
+    public static void main(String[] args) {
+
+        Caixa<String> caixaA = new Caixa<>();
+        caixaA.guardar("Aqora só aceita String"); // agora ele não aceita outro tipo senão String
+
+        String retornoSemCastParaString = caixaA.abrir();
+        System.out.println(retornoSemCastParaString);
+    }
+}
