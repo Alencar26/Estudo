d37506c53248237cde81d36cc9323f6fa6eb175e
Andre Alencar <aaugusto310@gmail.com>
2022-04-20 12:05:09 -0300

    Generics | java - exemplos | aula 1 | implementando exemplos com generics.

---
+++ java/Generics/src/generics/CaixaObjetoTeste.java
@@ -0,0 +1,20 @@
+package generics;
+
+public class CaixaObjetoTeste {
+
+    public static void main(String[] args) {
+
+        CaixaObjeto caixaA = new CaixaObjeto();
+        caixaA.guardar(2.3); //double -> Double
+
+        // o codigo abaixo vai dar problema pois o java espera um número inteiro para ser atribuido na variável
+        Double coisaA = (Double) caixaA.abrir(); //casting
+        System.out.println(coisaA);
+
+        CaixaObjeto caixaB = new CaixaObjeto();
+        caixaB.guardar("Olá, sou uma String");
+
+        String coisaB = (String) caixaB.abrir(); //casting
+        System.out.println(coisaB);
+    }
+}
