5c44c29a719fcd691833611db8cd11af34dbfde6
Andre Alencar <aaugusto310@gmail.com>
2022-05-02 10:43:55 -0300

    calculadora | swing | definição do gerenciador de layout sendo o BorderLayout, setado posição dos elementos em tela e criação da pasta View e classes correspondentes.

---
+++ projetos/calculadora_macOs_swing/src/br/com/al3ncar/calc/view/Botao.java
@@ -0,0 +1,6 @@
+package br.com.al3ncar.calc.view;
+
+import javax.swing.*;
+
+public class Botao extends JButton {
+}
---
+++ projetos/calculadora_macOs_swing/src/br/com/al3ncar/calc/view/Display.java
@@ -0,0 +1,11 @@
+package br.com.al3ncar.calc.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JPanel {
+
+    public Display() {
+        setBackground(Color.GREEN);
+    }
+}
