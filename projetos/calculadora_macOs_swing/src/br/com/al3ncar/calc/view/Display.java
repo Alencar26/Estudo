d415ff13ff5a3ebe7ac0ff39864c6368ae1a5260
Andre Alencar <aaugusto310@gmail.com>
2022-05-02 15:42:26 -0300

    calculadora | swing | Implementação do display, font, label, cores, alinhamento do label.

--- projetos/calculadora_macOs_swing/src/br/com/al3ncar/calc/view/Display.java
+++ projetos/calculadora_macOs_swing/src/br/com/al3ncar/calc/view/Display.java
@@ -5,7 +5,16 @@ import java.awt.*;
 
 public class Display extends JPanel {
 
+    private final JLabel label;
+
     public Display() {
-        setBackground(Color.GREEN);
+        setBackground(new Color(33,37,43));
+        label  = new JLabel("123,45");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25 ));
+
+        add(label);
     }
 }
--- projetos/calculadora_macOs_swing/src/br/com/al3ncar/calc/view/Teclado.java
+++ projetos/calculadora_macOs_swing/src/br/com/al3ncar/calc/view/Teclado.java
@@ -1,11 +1,10 @@
 package br.com.al3ncar.calc.view;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Teclado extends JPanel {
 
     public Teclado() {
-        setBackground(Color.RED);
+
     }
 }
