852d49e1607a6bff33a8663eb1e13a0fdb9317be
Andre Alencar <aaugusto310@gmail.com>
2022-05-04 02:52:53 -0300

    feat: mapeamento de todas as teclas da calculadora.

--- projetos/calculadora_macOs_swing/src/br/com/al3ncar/calc/model/Memoria.java
+++ projetos/calculadora_macOs_swing/src/br/com/al3ncar/calc/model/Memoria.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Memoria {
 
+    private enum TipoComando {
+            ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA;
+    };
+
     // USANDO PADRÃO DE PROJETO SINGLETON: só posso ter uma instância dessa classe.
 
     private static final Memoria instancia = new Memoria();
@@ -26,12 +30,44 @@ public class Memoria {
         return textoAtual.isEmpty() ? "0" : textoAtual;
     }
 
-    public void processarComando(String valor) {
-        if ("AC".equals(valor)){
+    public void processarComando(String texto) {
+
+        TipoComando tipoComando = detectarTipoComando(texto);
+        System.out.println(tipoComando);
+        if ("AC".equals(texto)){
             textoAtual = "";
         } else {
-            textoAtual += valor;
+            textoAtual += texto;
         }
         observadores.forEach(obs -> obs.valorAlterado(getTextoAtual()));
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+        if (textoAtual.isEmpty() && texto.equals("0")) {
+            return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        } catch (NumberFormatException e) {
+            // Quando não for número
+            if ("AC".equals(texto)) {
+                return TipoComando.ZERAR;
+            } else if ("/".equals(texto)) {
+                return  TipoComando.DIV;
+            } else if ("+".equals(texto)) {
+                return  TipoComando.SOMA;
+            } else if ("-".equals(texto)) {
+                return  TipoComando.SUB;
+            } else if ("x".equals(texto)) {
+                return  TipoComando.MULT;
+            } else if ("=".equals(texto)) {
+                return  TipoComando.IGUAL;
+            } else if (",".equals(texto)) {
+                return  TipoComando.VIRGULA;
+            }
+        }
+        return null;
     }
 }
