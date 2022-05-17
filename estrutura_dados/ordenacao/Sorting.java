7405711fc447cc3096cef95a70b5dfb79be0c30e
Andre Alencar <aaugusto310@gmail.com>
2022-05-17 20:20:35 -0300

    Estrutura de dados | Ordenação/Sroting | Ordenação por troca decrescente.

--- estrutura_dados/ordenacao/Sorting.java
+++ estrutura_dados/ordenacao/Sorting.java
@@ -4,10 +4,17 @@ public class Sorting {
     
     int[] v = {2, 45, 23, 1, 166, 3, 55, 1003, 30669, 18, 8, 319};
     ordenar(v);
-    
+    System.out.println("Ordenação Crescente:");
     for (int i = 0; i < v.length; i++) {
       System.out.println(v[i]);
     }
+
+    ordenarDesc(v);
+    System.out.println("\nOrdenação Decrescente:");
+    for (int i = 0; i < v.length; i++) {
+      System.out.println(v[i]);
+    }
+
   }
 
   public static void ordenar(int[] vetor) {
@@ -24,4 +31,20 @@ public class Sorting {
       }
     }
   }
+
+  public static void ordenarDesc(int[] vetor) {
+    
+    int n = vetor.length;
+    int temporario;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < n -1; j++) {
+        if (vetor[j] < vetor[j + 1]) {
+         temporario = vetor[j + 1];
+         vetor[j + 1] = vetor[j];
+         vetor[j] = temporario;
+        }
+      }
+    }
+  }
 }
+
