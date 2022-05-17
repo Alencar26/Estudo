225e82f73a789becc3795dc7ca2169751e5d281b
Andre Alencar <aaugusto310@gmail.com>
2022-05-17 20:53:30 -0300

    feat: Estrutura de dados | Ordenação por seleção | implementação de ordenação por seleção com inteiros em ordem crescente.

---
+++ estrutura_dados/ordenacao/OrdenacaoPorSelecao.java
@@ -0,0 +1,35 @@
+public class OrdenacaoPorSelecao {
+  
+  public static void main(String[] args) {
+    
+    int[] v = {2, 12, 96, 1, 99, 1006, 3, 17, 0, 100};
+    ordenarPorSelecao(v);
+    System.out.println("Ordenação por seleção:");
+    for (int i = 0; i < v.length; i++) {
+     System.out.println(v[i]); 
+    }
+  }
+
+  public static int seleciona(int[] v, int n) {
+   int  i = 0;
+   for (int j = 1; j < n; j++) {
+     if (v[i] < v[j]) {
+       i = j;
+     }
+  }
+  return i;
+}
+
+  public static void ordenarPorSelecao(int[] v) {
+    int n = v.length;
+    int i, j;
+    int temporario;
+    while (n > 1) {
+      i = seleciona(v, n);
+      temporario = v[n - 1];
+      v[n - 1] = v[i];
+      v[i] = temporario;
+      n--;
+    }
+  }
+}
