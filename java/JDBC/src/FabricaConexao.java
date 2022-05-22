cae8a20fd86c743935d534a8887f7d0920048b43
Andre Alencar <aaugusto310@gmail.com>
2022-05-22 15:44:05 -0300

    feture: JDBC | criando classe responsável por efetuar a conexão com o banco.

---
+++ java/JDBC/src/FabricaConexao.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+    public static Connection getConexao() {
+
+        try {
+            final String url = "jdbc:mysql://localhost:3307?verifyServerCertificate=false&useSSL=true";
+            final String user = "root";
+            final String password = "mysql";
+
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            // lançando exceção e tranformando em um runtimeException.
+            throw new RuntimeException(e);
+        }
+    }
+}
