eef22f8cc476269bbeeedfc8b8a0352886791799
Andre Alencar <aaugusto310@gmail.com>
2022-05-22 16:09:35 -0300

    refactor: JDBC | melhorando classe FabricaConexao para informar nome do banco em parâmetro do método getConexao.

--- java/JDBC/src/FabricaConexao.java
+++ java/JDBC/src/FabricaConexao.java
@@ -7,7 +7,21 @@ public class FabricaConexao {
     public static Connection getConexao() {
 
         try {
-            final String url = "jdbc:mysql://localhost:3307?verifyServerCertificate=false&useSSL=true";
+            final String url = "jdbc:mysql://localhost:3307/?verifyServerCertificate=false&useSSL=true";
+            final String user = "root";
+            final String password = "mysql";
+
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            // lançando exceção e tranformando em um runtimeException.
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConexao(String nomeDoBanco) {
+
+        try {
+            final String url = "jdbc:mysql://localhost:3307/"+nomeDoBanco+"?verifyServerCertificate=false&useSSL=true";
             final String user = "root";
             final String password = "mysql";
 
