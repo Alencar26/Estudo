7c180d28410cdfe19faac72ec44b24c58f14eb54
Andre Alencar <aaugusto310@gmail.com>
2022-05-23 14:39:43 -0300

    refactor: JDBC | Arquivo externo de conexão com banco de dados.

--- java/JDBC/src/FabricaConexao.java
+++ java/JDBC/src/FabricaConexao.java
@@ -1,18 +1,21 @@
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
     public static Connection getConexao() {
 
         try {
-            final String url = "jdbc:mysql://localhost:3307/?verifyServerCertificate=false&useSSL=true";
-            final String user = "root";
-            final String password = "mysql";
+            Properties prop = getProperties();
+            final String url = prop.getProperty("banco.url") + prop.getProperty("banco.params");
+            final String user = prop.getProperty("banco.user");
+            final String password = prop.getProperty("banco.password");
 
             return DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             // lançando exceção e tranformando em um runtimeException.
             throw new RuntimeException(e);
         }
@@ -21,14 +24,23 @@ public class FabricaConexao {
     public static Connection getConexao(String nomeDoBanco) {
 
         try {
-            final String url = "jdbc:mysql://localhost:3307/"+nomeDoBanco+"?verifyServerCertificate=false&useSSL=true";
-            final String user = "root";
-            final String password = "mysql";
+            Properties prop = getProperties();
+            final String url = prop.getProperty("banco.url") + nomeDoBanco + prop.getProperty("banco.params");
+            final String user = prop.getProperty("banco.user");
+            final String password = prop.getProperty("banco.password");
 
             return DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             // lançando exceção e tranformando em um runtimeException.
             throw new RuntimeException(e);
         }
     }
+
+    private static Properties getProperties() throws IOException {
+        Properties prop = new Properties();
+        final String PATH = "/conexao.properties";
+        prop.load(FabricaConexao.class.getResourceAsStream(PATH));
+        return prop;
+    }
 }
+
