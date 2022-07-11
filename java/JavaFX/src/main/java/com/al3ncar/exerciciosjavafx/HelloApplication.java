e4be9d1382e504006c5d0f219df152fd5ecbe15a
Andre Alencar <aaugusto310@gmail.com>
2022-07-11 16:36:42 -0300

    feat: Primeiro teste com javafx.

--- java/JavaFX/src/main/java/com/al3ncar/exerciciosjavafx/HelloApplication.java
+++ java/JavaFX/src/main/java/com/al3ncar/exerciciosjavafx/HelloApplication.java
@@ -1,19 +1,32 @@
 package com.al3ncar.exerciciosjavafx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage     stage) throws IOException {
+
+        Button b1 = new Button("A");
+        Button b2 = new Button("B");
+        Button b3 = new Button("C");
+
+        HBox box = new HBox();
+        box.setAlignment(Pos.CENTER);
+        box.setSpacing(10);
+        box.getChildren().add(b1);
+        box.getChildren().add(b2);
+        box.getChildren().add(b3);
+
+        Scene cena = new Scene(box, 150, 100);
+
+        stage.setScene(cena);
         stage.show();
     }
 
