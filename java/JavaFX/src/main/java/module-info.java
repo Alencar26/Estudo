9fe50e917190df783346e17467a8dd7ced12960f
Andre Alencar <aaugusto310@gmail.com>
2022-07-11 16:22:05 -0300

    feat: Iniciando projeto JavaFX.

---
+++ java/JavaFX/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+    module com.al3ncar.exerciciosjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.al3ncar.exerciciosjavafx
+            to javafx.fxml;
+    exports com.al3ncar.exerciciosjavafx;
+}
