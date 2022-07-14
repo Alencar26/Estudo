8414f1b3ca753753ee7bf0a055a02ebe24a53742
Andre Alencar <aaugusto310@gmail.com>
2022-07-14 14:41:06 -0300

    feat: começando os estilos da aplicação.

--- java/JavaFX/src/main/java/com/al3ncar/exerciciosjavafx/Contador.java
+++ java/JavaFX/src/main/java/com/al3ncar/exerciciosjavafx/Contador.java
@@ -9,6 +9,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Contador extends Application {
 
     private int  contador = 0;
@@ -17,7 +19,9 @@ public class Contador extends Application {
     public void start(Stage stage) throws Exception {
 
         Label lblTitulo = new Label("Contador");
+        lblTitulo.getStyleClass().add("titulo");
         Label lblNumero = new Label("0");
+        lblNumero.getStyleClass().add("numero");
 
         Button btnDecremento = new Button("-");
         btnDecremento.setOnAction(e -> {
@@ -38,15 +42,21 @@ public class Contador extends Application {
         boxBotoes.getChildren().add(btnIncremento);
 
         VBox boxPrincipal = new VBox();
+        boxPrincipal.getStyleClass().add("conteudo");
         boxPrincipal.setAlignment(Pos.CENTER);
         boxPrincipal.setSpacing(10);
         boxPrincipal.getChildren().add(lblTitulo);
         boxPrincipal.getChildren().add(lblNumero);
         boxPrincipal.getChildren().add(boxBotoes);
 
-        //String PathCSS = getClass().getResource("/com/al3ncar/exerciciosjavafx/Contador.css").toExternalForm();
+        String PathCSS = Objects
+                .requireNonNull(
+                    getClass()
+                            .getResource("/main/java/com/al3ncar/exerciciosjavafx/Contador.css"))
+                .toExternalForm();
         Scene cenaPrincipal = new Scene(boxPrincipal, 400 ,400);
-        //cenaPrincipal.getStylesheets().add(PathCSS);
+        cenaPrincipal.getStylesheets().add(PathCSS);
+        cenaPrincipal.getStylesheets().add("https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap");
 
         stage.setScene(cenaPrincipal);
         stage.show();
