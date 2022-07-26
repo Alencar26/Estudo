a4a6512f1e6382df84450e4ffbf75dc123ccdff6
Andre Alencar <aaugusto310@gmail.com>
2022-07-26 15:10:56 -0300

    feat: Métodos http com Spring Boot. Exemplos.

---
+++ java/SpringBoot/src/main/java/com/al3ncar/springboot/controllers/MetodosHttpController.java
@@ -0,0 +1,33 @@
+package com.al3ncar.springboot.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/metodos")
+public class MetodosHttpController {
+
+    @GetMapping
+    public String get() {
+        return "requisição GET";
+    }
+
+    @PostMapping
+    public String post() {
+        return "requisição POST";
+    }
+
+    @PutMapping
+    public String put() {
+        return "requisição PUT";
+    }
+
+    @PatchMapping
+    public String patch() {
+        return "requisição PATCH";
+    }
+
+    @DeleteMapping
+    public String delete() {
+        return "requisição DELETE";
+    }
+}
