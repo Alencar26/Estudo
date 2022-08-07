88129dd5ecf6f4e23a724ccc269ed132b8fd80db
Andre Alencar <aaugusto310@gmail.com>
2022-08-07 12:58:09 -0300

    feat: 02. Variáveis, String e concatenação.

---
+++ lua/variaveis.lua
@@ -0,0 +1,34 @@
+
+
+--[[
+-- Estudo de lua:
+--
+-- Nesse arquivo será visto:
+--
+-- 1. variáveis
+-- 2. concatenação
+-- 3. strings
+--]]
+
+olamundo = "Olá Mundo."
+print(olamundo)
+
+olamundo = 45
+print(olamundo)
+
+operacao = 45 * 10
+print(operacao)
+
+linha1 = "linha 1 "
+
+linha2 = "linha 2."
+
+-- concatenação
+print(linha1..linha2)
+
+print("para concatenar usa-se o operador ..")
+-- concatenando
+print("concatenando o txt com número > " .. 23)
+
+parte1 = "parte 1"
+print(parte1 .. " - " .. 23 .. " - parte 2")
