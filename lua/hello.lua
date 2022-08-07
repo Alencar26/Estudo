ba7caa61ff0684710ed5774744131c7a7f3b2c87
Andre Alencar <aaugusto310@gmail.com>
2022-08-07 12:40:24 -0300

    feat: 01. Início do estudo da linguagem LUA. Visto: operadores, string, números e função print().

---
+++ lua/hello.lua
@@ -0,0 +1,37 @@
+
+--[[
+--
+-- Iniciando estudos em lua para conseuir desenvolver scripst
+-- de forma rápida e fácil. 
+--
+-- Esse arquivo vai abordar os assuntos de:
+-- 
+-- 1. print em console;
+-- 2. string;
+-- 3. números;
+-- 4. comentários;
+-- 5. operadores bárico
+--
+--]]
+
+-- esse é um comentário
+
+-- printando no console
+print("Hello World!")
+
+-- print números
+print(12345678)
+
+print(1 + 3)
+print(2 - 4)
+print(3 * 3)
+print(12 / 3)
+
+-- mod
+print(9 % 2)
+
+-- Operadores de igualdade
+print(2 > 3)
+print(3 < 10)
+print(10 >= 11)
+print(19 <= 18)
