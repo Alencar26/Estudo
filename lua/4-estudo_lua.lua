4334930fc0c034fc91729a5902ac150e5ae12696
Andre Alencar <aaugusto310@gmail.com>
2022-08-09 09:45:25 -0300

    feat: 05. Entrada de dados com lua. io.read()

---
+++ lua/4-estudo_lua.lua
@@ -0,0 +1,16 @@
+
+
+-- Estudo de lua
+--
+-- Nesse arquivo será visto:
+--
+-- 1. Entrada e saída de dados no console.
+
+nome = nil
+print("Qual é seu nome?")
+--função para ler Entrada
+nome = io.read()
+print("Seu nome é: " .. nome)
+
+-- io = conjunto de funções relacionada a entrada e saída de dados.
+
