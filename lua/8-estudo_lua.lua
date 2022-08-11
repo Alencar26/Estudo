8b28157b805af3c6c9e2ba96596de655397f2e1d
Andre Alencar <aaugusto310@gmail.com>
2022-08-11 00:26:44 -0300

    feat: 010 - Lopp FOR e REPEATE UNTIL.

---
+++ lua/8-estudo_lua.lua
@@ -0,0 +1,30 @@
+
+
+-- ESTUDO DE LUA
+--
+-- Loop - FOR e REPEAT UNTIL
+
+
+for i = 1, 10, 1 do
+  print(i)
+end
+
+-- for increments de 2 em dois
+for i = 1, 11, 2 do
+  print(i)
+end
+
+for i = 1, 40, 5 do
+  print(i)
+end
+
+
+-- REPEAT
+-- é parecido com Do While
+indice = 1
+
+repeat
+  print(indice)
+until indice <= 10
+
+
