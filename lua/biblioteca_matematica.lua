26f8be22fbc674f1b85130618e4803c20a984fe5
Andre Alencar <aaugusto310@gmail.com>
2022-08-11 16:54:05 -0300

    feat: 012 - Introdução a biblioteca padão do lua.

---
+++ lua/biblioteca_matematica.lua
@@ -0,0 +1,44 @@
+
+
+-- ESTUDO DE LUA
+--
+-- Nessa arquivo será abordado alguns exemplos da biblioteca de matemática.
+
+
+-- arredondar um número
+--
+-- Arredonda para baixo
+print(math.floor(0.5))
+print(math.floor(1.6))
+print("")
+-- Arredonda para cima
+print(math.ceil(0.5))
+print(math.ceil(1.6))
+
+-- retorna o maior valor
+print(math.max(3, 6, 2, 7))
+
+
+--substituir string por outra
+print(string.gsub("olá mundo", "mundo", "game"))
+
+--substituir string e n ocorrencias
+-- código abaixo só vai realizar a troca nas duas primeiras ocorrencias
+print(string.gsub("Olá Mundo, Olá Mundo, Olá Mundo, Olá Mundo", "Mundo", "Cosmos", 2))
+
+
+-- manipulação de arquivos
+-- exemplo mapa de um jogo
+--      X = enimigo
+--      # = arbusto
+--      $ = cofre 
+--      = = caminho
+
+mapa = io.lines("mapa_exemplo.txt")
+for linha in mapa do
+   print(linha)
+end
+-- ou direto no for
+for linha in io.lines("mapa_exemplo.txt") do
+  print(linha)
+end
