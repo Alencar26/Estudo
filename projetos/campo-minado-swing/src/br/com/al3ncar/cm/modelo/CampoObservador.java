2f5d1a8ce4cebe896dec169119be7af3145bbf4c
Andre Alencar <aaugusto310@gmail.com>
2022-09-29 10:40:42 -0300

    remove: projetos e dasafios movidos para outro repositório no github. (Repositório Projetos)

--- projetos/campo-minado-swing/src/br/com/al3ncar/cm/modelo/CampoObservador.java
+++
@@ -1,7 +0,0 @@
-package br.com.al3ncar.cm.modelo;
-
-@FunctionalInterface
-public interface CampoObservador {
-
-    public void eventoOcorreu(Campo campo, CampoEvento evento);
-}
--- projetos/campo-minado/src/main/java/br/com/al3ncar/cm/excecao/SairException.java
+++
@@ -1,6 +0,0 @@
-package br.com.al3ncar.cm.excecao;
-
-public class SairException extends RuntimeException{
-
-    private static final long serialVersionUID = 1L;
-}
