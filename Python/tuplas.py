b6f3f8eb3b9180a89aed39a0396068397fcb23f2
Andre Alencar <aaugusto310@gmail.com>
2024-02-02 07:44:23 -0300

    feat: continuação do estudo de tuplas
    Foram abordados conversão de tupla para listas, tuplas com 1 unico
    valor, remoção de tuplas, concatenação e desempacotamento de
    tuplas/listas.

--- Python/tuplas.py
+++ Python/tuplas.py
@@ -37,3 +37,62 @@ print(type(exemplo)) # <class 'str'>
 um_valor = ("valor_unico",) #certo
 print(type(um_valor)) # <class 'tuple'>
 
+um_valor_new = "valor_unico", #certo <-- tbm é uma tupla
+print(type(um_valor_new)) # <class 'tuple'>
+
+tupla_nome = "nome1", "nome2", "nome3", "nome4" #certo <-- tbm é uma tupla
+print(type(tupla_nome)) # <class 'tuple'>
+
+#---- Tranformando tupla em lista - Add valor _ tranforma em tupla novamente
+
+tupla_itens = ("item1", "item2", "item3", "item4")
+print(tupla_itens)
+
+lista_itens = list(tupla_itens)
+print(lista_itens)
+
+lista_itens.append("item5")
+print(lista_itens)
+
+tupla_itens = tuple(lista_itens)
+print(tupla_itens)
+
+#--- REMOVE
+
+del tupla_itens
+
+#--- CONCATENAÇÃO
+
+tupla_a = ("itemA",)
+tupla_b = ("itemB", "itemC")
+
+print(tupla_a + tupla_b)
+print(tupla_a * 3)
+
+tupla_3x = tupla_a * 3
+
+for i in range(len(tupla_3x)):
+    print('-' * 7)
+    print(i, tupla_3x[i])
+    
+# --- desempacotar as variáveis - o mesmo vale para listas
+
+pacote = ("pão", "leite", "arroz", "pizza")
+
+(x, y, z, a) = pacote
+print(x)
+print(y)
+print(z)
+print(a)
+
+pao = pacote[0]
+print(pao)
+
+(x, *y) = pacote # x -> 1 item | y -> lista dos demais itens
+print("x:", x)
+print("y:", y)
+
+(x, *y, z) = pacote # x -> 1º item | y -> itens entre 1º e ultimo | z -> ultimo item
+print("x:", x)
+print("y:", y)
+print("z:", z)
