427b02b445a7d1f5d3ab06f002dc69f27b393397
Andre Alencar <aaugusto310@gmail.com>
2024-02-06 07:10:36 -0300

    feat: Argumentos Arbitrários: Args & Kwargs
    Exemplos de utilização de ambos os casos, quando não sabemos quantoos
    argumentos nossa função irá receber sejam elas nomeadas ou não.

--- Python/fundamento/argumentos_nomeados.py
+++ Python/fundamento/argumentos_nomeados.py
@@ -33,3 +33,38 @@ def print_imovel(descricao_imovel, n_quartos, vagasGaragem=None):
         
 print_imovel('Casa com 2 quartos', 2, 3)
 print_imovel('Casa com 1 quarto', 1)
+
+"""
+ARGUMENTO ARBITRÁRIO - ARGS
+Quando não sabemos o número de argumentos que serão passados para a função
+"""
+
+def print_names(*args):
+    print(args)
+    
+print_names('João', 'Maria', 'Pedro') # imprime um tupla com os argumentos
+
+lista_nomes = ['João', 'Maria', 'Pedro']
+print_names(lista_nomes) # imprime uma lista com os argumentos
+print_names(*lista_nomes) # o * é usado para desempacotar a lista
+
+
+"""
+ARGUMENTO ARBITRÁRIO - KWARGS
+Quando não sabemos o nome dos argumentos que serão passados para a função,
+a principal diferença entre os dois exemplos acima é que o primeiro utiliza 
+um dicionário para receber os argumentos, e o segundo utiliza um *args para receber
+os argumentos.
+"""
+
+def print_names(**kwargs):
+    print(kwargs)
+
+print_names(name='João', last_name='Silva', age=30) # imprime um dicionário com os argumentos
+
+def print_sala_de_aula(professora,**alunos):
+    print(f'Professora: {professora}')
+    for aluno, nota in alunos.items():
+        print(f'Aluno: {aluno}, nota: {nota}')
+
+print_sala_de_aula('Maria', João=10, Pedro=9, Ana=8)
