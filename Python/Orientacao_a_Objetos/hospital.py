50b7ef291ce90095a15945492139afd0f1080c3a
Andre Alencar <aaugusto310@gmail.com>
2024-02-13 18:17:00 -0300

    feat: Modificadores de acesso Public, Private e Protected no python
    Entendendo e dando exemplo de como usar os Modificadores de acesso no
    python.
    
    Private: __variavel
    Protected: _variavel
    Public: variavel

--- Python/Orientacao_a_Objetos/hospital.py
+++ Python/Orientacao_a_Objetos/hospital.py
@@ -3,15 +3,22 @@ from datetime import date
 class Paciente:
     
     def __init__(self, nome, idade, cpf, email) -> None:
-        self.nome = nome
-        self.idade = idade
-        self.cpf = cpf
-        self.email = email
+        self.__nome = nome  #private
+        self.idade = idade  #public
+        self.cpf = cpf      #public
+        self._email = email #protected
         
     @classmethod
     def criar_paciente(cls, nome, ano_nascimento, cpf, email):
         idade = date.today().year - ano_nascimento
         return cls(nome, idade, cpf, email)
+    
+    def retornaNome(self):
+        self.__metodo_privado() #chamando o método privado dentro do método publico
+        return self.__nome
+    
+    def __metodo_privado(self):
+        print('Este método é privado')
     
 class Medico:
     pass
@@ -19,3 +26,5 @@ class Medico:
 paciente_01 = Paciente.criar_paciente('João', 1990, '123.456.789-10', 'XXXXXXXXXXXXXX')
 print(paciente_01.__dict__)
 print(paciente_01.idade)
+
+print(paciente_01.retornaNome())
