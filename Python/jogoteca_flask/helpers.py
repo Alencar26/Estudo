0bde1610068b6b45fee7fe36d717867be1ce3de2
Andre Alencar <aaugusto310@gmail.com>
2024-03-24 01:44:53 -0300

    refator: Ajuste para excluir capas repetidar e adicionar timestamp em cada capa para evitar prblema de cache
    Foi add sequencial único em cada capa para avitar prblema de cache no
    navegador quando houver alteração da capa.
    Foi ajustado também a exclusão de capas repetidas, como o timestamp
    deixa a img com nome único, tive que ajustar a função para identificar
    as imagens e excluir antes de salvar a nova imagem.

--- Python/jogoteca_flask/helpers.py
+++ Python/jogoteca_flask/helpers.py
@@ -1,14 +1,22 @@
 import os
 from app import app
+from time import time
+from fnmatch import filter
 
 __img_path = app.config['UPLOAD_PATH']
 
 def recuperar_imagem(id):
     for nome_arquivo in os.listdir(__img_path):
-        if f'capa{id}.jpg' == nome_arquivo: 
+        if f'capa{id}' in nome_arquivo: 
             return nome_arquivo
     return 'default.png'
 
 def salva_imagem(jogo, img):
     if img.filename != '':
-        img.save(f'{__img_path}/capa{jogo.id}.jpg')
+        if recuperar_imagem(jogo.id) == 'default.png':
+            img.save(f'{__img_path}/capa{jogo.id}-{time()}.jpg')
+        else:
+            capas = filter(os.listdir(__img_path), f'capa{jogo.id}*')
+            for capa in capas:
+                os.remove(f'{__img_path}/{capa}')
+            img.save(f'{__img_path}/capa{jogo.id}-{time()}.jpg')
