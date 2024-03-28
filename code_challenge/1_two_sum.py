dbab05ec4d52be72a032edab07ff5a91bf79ec58
Andre Alencar <aaugusto310@gmail.com>
2024-03-28 12:22:41 -0300

    Challange: 01.twoSum - Desafio de código com cmplexidade O(n) v4

--- code_challenge/1_two_sum.py
+++ code_challenge/1_two_sum.py
@@ -1,9 +1,9 @@
 """sumary_line
 
-Challenge: Given an array of integers nums and an integer target, 
+Challenge: Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, 
+You may assume that each input would have exactly one solution,
 and you may not use the same element twice.
 
 You can return the answer in any order.
@@ -32,41 +32,59 @@ Only one valid answer exists.
 GOAL: Can you come up with an algorithm that is less than O(n²) time complexity?
 """
 
+from typing import List
 
-
-from typing import Dict, List
 
 # O(n²)
 def twoSum_v1(nums: List[int], target: int) -> List[int]:
-        result: List[int] = []
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                if target == (nums[i] + nums[j]):
-                    result.append(i)
-                    result.append(j)
-        return result
-
-print(twoSum_v1([3,2,3], 6))
-
-# O(n)
-def twoSum_v2(nums: List[int], target: int) -> List[int]:
-        result: List[int] = []
-        for i in range(0, len(nums)):
-            if i+1 < len(nums) and target == (nums[i] + nums[i+1]):
+    result: List[int] = []
+    for i in range(0, len(nums)):
+        for j in range(i + 1, len(nums)):
+            if target == (nums[i] + nums[j]):
                 result.append(i)
-                result.append(i+1)
-        return result
+                result.append(j)
+    return result
 
-print(twoSum_v2([3,2,3], 6))
+
+print(twoSum_v1([3, 2, 3], 6))
+
+
+# O(n) - falho
+def twoSum_v2(nums: List[int], target: int) -> List[int]:
+    result: List[int] = []
+    for i in range(0, len(nums)):
+        if i + 1 < len(nums) and target == (nums[i] + nums[i + 1]):
+            result.append(i)
+            result.append(i + 1)
+    return result
+
+
+print(twoSum_v2([3, 2, 3], 6))
+
 
 # O(n)
 def twoSum_v3(nums: List[int], target: int) -> List[int]:
-        num_dict: dict = {}
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in num_dict:
-                return [num_dict[diff], i]
-            num_dict[num] = i
-        return []
+    num_dict: dict = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in num_dict:
+            return [num_dict[diff], i]
+        num_dict[num] = i
+    return []
 
-print(twoSum_v3([3,2,3], 6))
+
+print(twoSum_v3([11, 3, 7, 2, 5], 9))
+
+
+def twoSum_v4(nums: List[int], target: int) -> List[int] | None:
+    hashmap: dict = dict()
+    position: int = 0
+    while position < len(nums):
+        if (target - nums[position]) not in hashmap:
+            hashmap[nums[position]] = position
+            position += 1
+        else:
+            return [hashmap[target - nums[position]], position]
+    return None
+
+print(twoSum_v4([11, 3, 7, 2, 5], 9))
