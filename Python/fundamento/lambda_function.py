0393a8f5c7faf96e3edded9d9ae8e21f4b17a5ec
Andre Alencar <aaugusto310@gmail.com>
2024-04-07 00:55:25 -0300

    feat: Lambda Function em Python

---
+++ Python/fundamento/lambda_function.py
@@ -0,0 +1,11 @@
+
+somar_com_10 = lambda valor: valor + 10
+
+result: int = somar_com_10(5)
+print(result)
+
+somar = lambda x, y: x + y
+print(somar(5, 5))
+
+par_ou_impar = lambda valor: 'Par' if valor % 2 == 0 else 'Impar'
+print(par_ou_impar(2))
