28094c498b8d4270b5d664b3549453f8a14824e2
Andre Alencar <aaugusto310@gmail.com>
2024-04-13 20:17:43 -0300

    feat: entendendo composer no PHP

---
+++ PHP/composer-php/src/Buscador.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+
+    private $httpClient;
+    private $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url): array
+    {
+        $response = $this->httpClient->request('GET', $url);
+
+        $html = $response->getBody();
+
+        $this->crawler->addHtmlContent($html);
+        $elementos = $this->crawler->filter('span.card-curso__nome');
+        $ementosCursos = [];
+
+        foreach ($elementos as $elemento)
+        {
+            $ementosCursos[] = $elemento->textContent;
+        }
+
+        return $ementosCursos;
+    }
+}
