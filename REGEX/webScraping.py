2c907bd2e5c4fb09fc8869b7caa77354253a6847
Andre Alencar <aaugusto310@gmail.com>
2024-05-09 23:27:47 -0300

    feat: REGEX, aprendendo sobre backreference

--- REGEX/webScraping.py
+++ REGEX/webScraping.py
@@ -5,10 +5,14 @@ url = "https://monicahillman.github.io/monibank/"
 response = requests.get(url)
 html = response.text
 
-# [^>] --> classe Negando do sinal de '>' REGEX não pega esse caracter
+# [^>] --> classe Negando tudo que vem depois do <(h[1-6])
 # (.*?) --> o sinal de '?' depois do '*' deixa o REGEX 'Lazy', ele só vai pegar a primeiro ocorrência
 regex = r'<(h[1-6])[^>]*>(.*?)<\/(h[1-6])>'
 
-titulos = re.findall(regex, html, re.DOTALL)
+# \1 ---> "backreference", para não repetir o pardrão como a regex de cima
+# podemos usar o backreference para "espelhar" o primeiro padrão no final da regex
+regex2 = r'<(h[1|2])[^>]*>(.*?)<\/\1>'
+
+titulos = re.findall(regex2, html, re.DOTALL)
 for titulo_html in titulos:
   print(titulo_html)
