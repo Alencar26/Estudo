f6c659cce5f2604d203b83a84bffde67bf68f615
Alencar26 <aaugusto310@gmail.com>
2025-02-22 00:51:17 -0300

    feat(GO) Maps

---
+++ GO/map/map.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	salario := map[string]int{"Andre": 1000, "João": 2000, "Pedro": 1500}
+
+	fmt.Println(salario["Pedro"])
+
+	frutas := map[string]string{
+		"Vermelho": "Maça",
+		"Amarelo":  "Banana",
+		"Verde":    "Melancia",
+		"Roxo":     "Uva",
+	}
+
+	//deletar valor
+	delete(frutas, "Amarelo")
+
+	//Adicionar valor
+	frutas["Preta"] = "Jabuticaba"
+
+	//Imprimir valor
+	fmt.Println(frutas["Preta"])
+
+	//Formas de criar map vazio
+	carros := make(map[string]string)
+	motots := map[string]string{}
+
+	fmt.Println(carros)
+	fmt.Println(motots)
+
+	//percorrendo map
+	for cor, fruta := range frutas {
+		fmt.Println(cor+":", fruta)
+	}
+}
