fe5e4d92124591a5aa61c79cd282771c7c3b1c51
Alencar26 <aaugusto310@gmail.com>
2025-03-02 16:11:27 -0300

    feat(GO) Server HTTP - Header, StatusCode, Query Parameters, json.

--- GO/serverHTTP/main.go
+++ GO/serverHTTP/main.go
@@ -8,5 +8,22 @@ func main() {
 }
 
 func hello(resp http.ResponseWriter, req *http.Request) {
-	resp.Write([]byte("Olá Mundo!"))
+	if req.URL.Path != "/" {
+		resp.WriteHeader(http.StatusNotFound)
+		resp.Write([]byte("Roda não existe."))
+		return
+	}
+
+	//Query Parameters. Ex: localhost:8080?nome=André
+	nomeParam := req.URL.Query().Get("nome")
+	if nomeParam == "" {
+		resp.Header().Set("Content-Type", "application/json")
+		resp.WriteHeader(http.StatusBadRequest)
+		resp.Write([]byte(`{"mensagem":"Nome ausente.","status": "400 Bad Request"}`))
+		return
+	}
+
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusOK)
+	resp.Write([]byte(`{"mensagem":"Olá ` + nomeParam + `!","status": "200 OK"}`))
 }
