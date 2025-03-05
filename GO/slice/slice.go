b1c9d758ac7f0e87643cc5f5ad97cadb828e7a26
Alencar26 <aaugusto310@gmail.com>
2025-03-04 22:49:54 -0300

    feat(GO) Slice - Juntado dois slice em um único (append).

--- GO/slice/slice.go
+++ GO/slice/slice.go
@@ -39,4 +39,13 @@ func main() {
 	for _, nome := range carros {
 		println(nome)
 	}
+
+	slice1 := []int{1, 2, 3, 4, 5}
+	fmt.Println(slice1[:])
+	slice2 := []int{6, 7, 8, 9, 10}
+	fmt.Println(slice2[:])
+
+	slice1 = append(slice1, slice2...)
+	fmt.Println(slice1[:])
+
 }
