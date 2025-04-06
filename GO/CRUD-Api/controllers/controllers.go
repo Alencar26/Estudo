b2b925af81e0a9829279187133d9663c039e0a15
Alencar26 <aaugusto310@gmail.com>
2025-04-06 06:24:47 -0300

    feat(GO) CRUD-Api - Criação do Método POST, PUT e DELETE. Finalizando as operações do CRUD.

--- GO/CRUD-Api/controllers/controllers.go
+++ GO/CRUD-Api/controllers/controllers.go
@@ -28,3 +28,45 @@ func GetPersonalidadeById(w http.ResponseWriter, r *http.Request) {
 	database.DB.First(&personalidade, id)
 	json.NewEncoder(w).Encode(personalidade)
 }
+
+func CreatePersonalidade(w http.ResponseWriter, r *http.Request) {
+	var newPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&newPersonalidade)
+	database.DB.Create(&newPersonalidade)
+	json.NewEncoder(w).Encode(newPersonalidade)
+}
+
+func DeletePersonalidadeById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var personalidade models.Personalidade
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode("Personalidade deletada com sucesso.")
+}
+
+// func EditPersonalidadeById(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	id := vars["id"]
+
+// 	var personalidade models.Personalidade
+// 	database.DB.First(&personalidade, id)
+
+// 	var updatePersonalidade models.Personalidade
+// 	json.NewDecoder(r.Body).Decode(&updatePersonalidade)
+
+// 	database.DB.Model(&personalidade).Updates(updatePersonalidade)
+// 	json.NewEncoder(w).Encode("Personalidade atualizada com sucesso.")
+// }
+
+func EditPersonalidadeById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+
+	json.NewDecoder(r.Body).Decode(&personalidade)
+	database.DB.Save(&personalidade)
+	json.NewEncoder(w).Encode("Personalidade atualizada com sucesso.")
+}
--- GO/CRUD-Api/routes/routes.go
+++ GO/CRUD-Api/routes/routes.go
@@ -12,6 +12,9 @@ func HandlerRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.GetAllPersonalidades).Methods(http.MethodGet)
+	r.HandleFunc("/api/personalidades", controllers.CreatePersonalidade).Methods(http.MethodPost)
 	r.HandleFunc("/api/personalidades/{id}", controllers.GetPersonalidadeById).Methods(http.MethodGet)
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletePersonalidadeById).Methods(http.MethodDelete)
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditPersonalidadeById).Methods(http.MethodPut)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
