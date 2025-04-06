4d6b8a089bc3617b751987be7a0089263ab3b026
Alencar26 <aaugusto310@gmail.com>
2025-04-06 06:51:34 -0300

    feat(GO) CRUD-Api - Adicionando configuração do CORS para que a API possa se comunicar com outras aplicações.

--- GO/CRUD-Api/routes/routes.go
+++ GO/CRUD-Api/routes/routes.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -18,5 +19,12 @@ func HandlerRequest() {
 	r.HandleFunc("/api/personalidades/{id}", controllers.GetPersonalidadeById).Methods(http.MethodGet)
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletePersonalidadeById).Methods(http.MethodDelete)
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditPersonalidadeById).Methods(http.MethodPut)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
+	//CORS permite que qualquer origem acesse a API
+	// handlers.AllowedOrigins([]string{"*"}): Permite que qualquer origem acesse a API
+	// handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}): Permite que qualquer método HTTP acesse a API
+	// handlers.AllowedHeaders([]string{"Content-Type"}): Permite que qualquer cabeçalho HTTP acesse a API
+	// handlers.AllowCredentials(): Permite que credenciais sejam enviadas na requisição
+	// handlers.MaxAge(3600): Define o tempo máximo em segundos que o navegador deve armazenar as informações de CORS
+	// handlers.ExposeHeaders([]string{"Content-Type"}): Permite que o cabeçalho Content-Type seja exposto na resposta
 }
