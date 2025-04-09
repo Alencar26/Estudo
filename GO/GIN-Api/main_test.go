530e948420921dc7cffabf1d1df0f79a0c0da4e6
Alencar26 <aaugusto310@gmail.com>
2025-04-09 13:34:28 -0300

    test(GO) - CRUD-Api+Gin - Adicionando mais testes e mode de visualização dos logs.

--- GO/GIN-Api/main_test.go
+++ GO/GIN-Api/main_test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"GIN-Api/database"
 	"GIN-Api/handles"
+	"GIN-Api/models"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,9 +15,26 @@ import (
 )
 
 func SetupRoutesTest() *gin.Engine {
+	gin.SetMode(gin.TestMode)
 	r := gin.Default()
 
 	return r
+}
+
+var idAluno int
+
+func criarAlunoMock() {
+	aluno := models.Aluno{
+		Nome: "Aluno Teste Automatizado",
+		CPF:  "12345678900",
+		RG:   "123456789",
+	}
+	database.DB.Create(&aluno)
+	idAluno = int(aluno.ID)
+}
+
+func deletaAlunoMock() {
+	database.DB.Delete(&models.Aluno{}, idAluno)
 }
 
 func TestVerificaSaudacaoStatusCode(t *testing.T) {
@@ -41,13 +61,36 @@ func TestVerificaSaudacaoBody(t *testing.T) {
 
 func TestGetAllAlunos(t *testing.T) {
 	database.ConnectDB()
+	criarAlunoMock()
+	defer deletaAlunoMock()
 	r := SetupRoutesTest()
 	r.GET("/alunos", handles.GetAllAlunos)
 
-	req, _ := http.NewRequest("GET", "/alunos", nil)
+	req, _ := http.NewRequest(http.MethodGet, "/alunos", nil)
 	response := httptest.NewRecorder()
 	r.ServeHTTP(response, req)
 
 	assert.Equal(t, http.StatusOK, response.Code)
 	assert.NotEqual(t, "[]", response.Body.String(), "Retorno não deve ser vazio.")
+}
+
+func TestGetAlunoById(t *testing.T) {
+	database.ConnectDB()
+	criarAlunoMock()
+	defer deletaAlunoMock()
+
+	r := SetupRoutesTest()
+	r.GET("/alunos/:id", handles.GetAluno)
+
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/alunos/%d", idAluno), nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, request)
+
+	var aluno models.Aluno
+	if err := json.NewDecoder(response.Body).Decode(&aluno); err != nil {
+		t.Errorf("Erro ao decodificar resposta JSON: %v", err)
+	}
+
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, idAluno, int(aluno.ID), "Id deve ser igual ao id do aluno criado")
 }
