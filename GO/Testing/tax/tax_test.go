5502cb0f68d4d5354b8d838a3ab4718e8b90abb6
Alencar26 <aaugusto310@gmail.com>
2025-06-11 15:50:05 -0300

    feat(GO) Testing - Cobertura de teste com GO, saída de 100% de cobertura no terminal e visualização em HTML da cobertura de teste.

--- GO/Testing/tax/tax_test.go
+++ GO/Testing/tax/tax_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+//COMANDOS
+//  go test | Só executa os testes sem detalhes
+//  go test -v | Execução dos testes com detalhes das funções testadas
+//  go test -coverprofile=coverage.out | Executa os testes e mostra a cobertura de teste no código
+//  go tool cover -html=coverage.out | Vai exportar para HTML o código que está coberto por testes e mostrar o que não está coberto.
+
 func TestCalculateTax(t *testing.T)  {
   amount := 500.0
   expcted := 5.0
@@ -25,6 +31,7 @@ func TestCalculateTaxBatch(t * testing.T) {
     {1000.0, 10.0},
     {600.0, 5.0},
     {1300.0, 10.0},
+    {0.0, 0.0},
   }
 
   for _, item := range table {
