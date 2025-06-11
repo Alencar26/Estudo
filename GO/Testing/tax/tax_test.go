e740c6b03d0bc394143a096421a9a9e206e9bbc5
Alencar26 <aaugusto310@gmail.com>
2025-06-11 15:40:15 -0300

    feat(GO) Testing - Teste em batch com go nativo sem lib externa.

--- GO/Testing/tax/tax_test.go
+++ GO/Testing/tax/tax_test.go
@@ -14,3 +14,23 @@ func TestCalculateTax(t *testing.T)  {
     t.Errorf("Expected %f but got %f", expcted, result)
   }
 }
+
+func TestCalculateTaxBatch(t * testing.T) {
+  type calcTax struct {
+    amount, expcted float64
+  }
+
+  table := []calcTax{
+    {500.0, 5.0},
+    {1000.0, 10.0},
+    {600.0, 5.0},
+    {1300.0, 10.0},
+  }
+
+  for _, item := range table {
+    result := CalculateTax(item.amount)
+    if result != item.expcted {
+     t.Errorf("Expected %f but got %f", item.expcted, result)
+    }
+  }
+}
