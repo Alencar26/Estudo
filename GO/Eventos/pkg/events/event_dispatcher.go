c3a0a9ad88ecba0ff128e87206295b3e61f00461
Alencar26 <aaugusto310@gmail.com>
2025-08-13 00:15:51 -0300

    Feature(GO) Consumindo Eventos com Go - Sistema completo de manipulação e execução de eventos com Golang.

---
+++ GO/Eventos/pkg/events/event_dispatcher.go
@@ -0,0 +1,64 @@
+package events
+
+import "errors"
+
+var ErrHandlerAlreadyRegister = errors.New("handler already registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrHandlerAlreadyRegister
+			}
+		}
+	}
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
+
+func (ed *EventDispatcher) Clear() {
+	ed.handlers = make(map[string][]EventHandlerInterface)
+}
+
+func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	if _, has := ed.handlers[eventName]; has {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (ed *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ed.handlers[event.GetName()]; ok {
+		for _, handler := range handlers {
+			handler.Handle(event)
+		}
+	}
+	return nil
+}
+
+func (ed *EventDispatcher) Remove(evetName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[evetName]; ok {
+		for i, h := range ed.handlers[evetName] {
+			if h == handler {
+				ed.handlers[evetName] = append(ed.handlers[evetName][:i], ed.handlers[evetName][i+1:]...)
+				return nil
+			}
+		}
+	}
+	return nil
+}
---
+++ GO/Eventos/pkg/events/event_dispatcher_test.go
@@ -0,0 +1,161 @@
+package events
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type TestEvent struct {
+	Name     string
+	Patyload interface{}
+}
+
+func (e *TestEvent) GetName() string {
+	return e.Name
+}
+
+func (e *TestEvent) GetPayload() interface{} {
+	return e.Patyload
+}
+
+func (e *TestEvent) GetDateTime() time.Time {
+	return time.Now()
+}
+
+type TestEventHandler struct {
+	ID int
+}
+
+func (h *TestEventHandler) Handle(event EventInterface) {
+
+}
+
+type EventDispatcherTestSuite struct {
+	suite.Suite
+	event           TestEvent
+	event2          TestEvent
+	handler         TestEventHandler
+	handler2        TestEventHandler
+	handler3        TestEventHandler
+	eventDispatcher *EventDispatcher
+}
+
+func (s *EventDispatcherTestSuite) SetupTest() {
+	s.eventDispatcher = NewEventDispatcher()
+	s.event = TestEvent{Name: "Event", Patyload: "TesteEvent"}
+	s.event2 = TestEvent{Name: "Event2", Patyload: "TesteEvent2"}
+	s.handler = TestEventHandler{ID: 1}
+	s.handler2 = TestEventHandler{ID: 2}
+	s.handler3 = TestEventHandler{ID: 3}
+}
+
+func (s *EventDispatcherTestSuite) TestEventDispatcher_Register() {
+	err := s.eventDispatcher.Register(s.event.Name, &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	err = s.eventDispatcher.Register(s.event.Name, &s.handler2)
+	s.Nil(err)
+	s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	assert.Equal(s.T(), &s.handler, s.eventDispatcher.handlers[s.event.GetName()][0])
+	assert.Equal(s.T(), &s.handler2, s.eventDispatcher.handlers[s.event.GetName()][1])
+}
+
+func (s *EventDispatcherTestSuite) TestEventDispatcher_Register_WithSameHandler() {
+	err := s.eventDispatcher.Register(s.event.Name, &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	err = s.eventDispatcher.Register(s.event.Name, &s.handler)
+	s.Equal(ErrHandlerAlreadyRegister, err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+}
+
+func (s *EventDispatcherTestSuite) TestEventDispatcher_Clear() {
+	//Event1
+	err := s.eventDispatcher.Register(s.event.Name, &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	err = s.eventDispatcher.Register(s.event.Name, &s.handler2)
+	s.Nil(err)
+	s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	//Event2
+	err = s.eventDispatcher.Register(s.event2.Name, &s.handler3)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event2.GetName()]))
+
+	s.eventDispatcher.Clear()
+	s.Equal(0, len(s.eventDispatcher.handlers))
+}
+
+func (s *EventDispatcherTestSuite) TestEventDispatcher_Has() {
+	//Event1
+	err := s.eventDispatcher.Register(s.event.Name, &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	err = s.eventDispatcher.Register(s.event.Name, &s.handler2)
+	s.Nil(err)
+	s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	assert.True(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler))
+	assert.True(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler2))
+	assert.False(s.T(), s.eventDispatcher.Has(s.event.GetName(), &s.handler3))
+}
+
+type MockedEventHandler struct {
+	mock.Mock
+}
+
+func (m *MockedEventHandler) Handle(event EventInterface) {
+	m.Called(event)
+}
+
+func (s *EventDispatcherTestSuite) TestEventDispatcher_Dispatch() {
+	eh := &MockedEventHandler{}
+	eh.On("Handle", &s.event)
+	s.eventDispatcher.Register(s.event.GetName(), eh)
+	s.eventDispatcher.Dispatch(&s.event)
+	eh.AssertExpectations(s.T())
+	eh.AssertNumberOfCalls(s.T(), "Handle", 1)
+}
+
+func (s *EventDispatcherTestSuite) TestEventDispatcher_Remove() {
+	//Event1
+	err := s.eventDispatcher.Register(s.event.GetName(), &s.handler)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	err = s.eventDispatcher.Register(s.event.GetName(), &s.handler2)
+	s.Nil(err)
+	s.Equal(2, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	//Event2'
+	err = s.eventDispatcher.Register(s.event2.GetName(), &s.handler3)
+	s.Nil(err)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event2.GetName()]))
+
+	//REMOVE HANDLER1
+	s.eventDispatcher.Remove(s.event.GetName(), &s.handler)
+	s.Equal(1, len(s.eventDispatcher.handlers[s.event.GetName()]))
+	assert.Equal(s.T(), &s.handler2, s.eventDispatcher.handlers[s.event.GetName()][0])
+
+	//REMOVE HANDLER2
+	s.eventDispatcher.Remove(s.event.GetName(), &s.handler2)
+	s.Equal(0, len(s.eventDispatcher.handlers[s.event.GetName()]))
+
+	//REMOVE HANDLER3
+	s.eventDispatcher.Remove(s.event2.GetName(), &s.handler3)
+	s.Equal(0, len(s.eventDispatcher.handlers[s.event2.GetName()]))
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(EventDispatcherTestSuite))
+}
