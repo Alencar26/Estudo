1224cea782ae021f8d0b6bb541ec9e9d30a318a0
Alencar26 <aaugusto310@gmail.com>
2025-08-14 07:36:14 -0300

    Feature(GO) RabbitMQ - Criando comunicação com RabbitMQ e implementando função para consumir uma fila.

---
+++ GO/Eventos/pkg/rabbitmq/rabbitmq.go
@@ -0,0 +1,37 @@
+package rabbitmq
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+func OpenChannel() (*amqp.Channel, error) {
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	return ch, nil
+}
+
+func Consume(ch *amqp.Channel, out chan<- amqp.Delivery) error {
+	msgs, err := ch.Consume(
+		"minhafila",   //nome da fila que quero consumir
+		"go-consumer", //nome da aplicação que vai consumir
+		false,         //autoAck - Quando a msg é lida o rabbitmq já dá baixa na msg e remove da fila
+		false,         //fila exclusiva
+		false,         //noLocal
+		false,         //noWait
+		nil,           //args amqp.Table
+	)
+	if err != nil {
+		return err
+	}
+
+	for msg := range msgs {
+		out <- msg
+	}
+	return nil
+}
