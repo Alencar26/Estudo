3014c98ff2458d4b3d01e3abba3c760b69f0fead
Alencar26 <aaugusto310@gmail.com>
2025-09-03 08:04:50 -0300

    Feature(GO) GraphQL - Relacionamento de banco de dados com GraphQL.

--- GO/GraphQL/internal/database/course.go
+++ GO/GraphQL/internal/database/course.go
@@ -50,3 +50,22 @@ func (c *Course) FindAll() ([]Course, error) {
 	}
 	return courses, nil
 }
+
+func (c *Course) FindByCategoryId(categoryId string) ([]Course, error) {
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses WHERE category_id=$1;", categoryId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	courses := []Course{}
+	for rows.Next() {
+		var id, name, description, catId string
+		err := rows.Scan(&id, &name, &description, &catId)
+		if err != nil {
+			return nil, err
+		}
+		courses = append(courses, Course{ID: id, Name: name, Description: description, CategoryID: catId})
+	}
+	return courses, nil
+}
