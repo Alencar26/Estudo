dd6290d8445ef89b080c00116fbf5db0eaf89ecf
Alencar26 <aaugusto310@gmail.com>
2025-10-31 06:34:45 -0300

    Feature(GO) Cobra CLI - Utilizando Flags para receber dados pela linha de comando.

--- GO/CLI/README.md
+++ GO/CLI/README.md
@@ -15,6 +15,12 @@ Temos **três elementos principais** ao trabalhar com cobra:
 1. Chamada de Execução
 2. Comando
 3. Flags configuradas na inicialização - `Feito atravez de uma função Init()`
+
+PS1: Flags só são visíveis no comando onde ela foi criada 
+PS2: As PersistentFlags também aparecem em subcomandos ou comandos filhos
+
+
+### Criar Comandos
  
 Para adicionar comando ao nosso CLI usamos o comando abaixo:
 
@@ -28,3 +34,10 @@ cobra-cli add ping
 O comando acima vai gerar uma estrutura de código já com o padrão do cobra para nós implementarmos nossa funcionalidade de forma fácil e rápida.
 Nassa estrutura dentro de `cmd/nomeComando.go`, é possível adicionar nossa funcionalidade.
 
+### Criar Comando filhos
+
+Para criar comando filhos devemos usar:
+```bash
+#precisa do Cmd ao final
+cobra-cli add nomeComandoFilho -p 'nomecomandopaiCmd'
+```
---
+++ GO/CLI/cmd/create.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		existe, _ := cmd.Flags().GetBool("existe")
+		fmt.Printf("Comando existe: %t", existe)
+
+		fmt.Println("---------------------")
+
+		quantidade, _ := cmd.Flags().GetInt64("quantidade")
+		fmt.Printf("Quantidade informada é: %d", quantidade)
+	},
+}
+
+func init() {
+	categoryCmd.AddCommand(createCmd)
+	categoryCmd.PersistentFlags().String("name", "", "Flag também aparecem em comandos filhos")
+	categoryCmd.PersistentFlags().StringP("name2", "n", "SEM NOME", "Flag tipo StringP para ter um comando menor")
+	categoryCmd.PersistentFlags().BoolP("existe", "e", true, "Verifica se comando existe.")
+	categoryCmd.PersistentFlags().Int64P("quantidade", "q", 0, "Informe um Int64")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
